205e791c7502cfd3744d2cf2d80c348efb5b42c6
Shivam Bhardwaj <curious.antimony@gmail.com>
2025-11-24 15:12:22 -0800

    Remove duplicate wasm_bindgen import causing error

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -7,7 +7,6 @@ use components::canvas::SimulationCanvas;
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
-use wasm_bindgen::prelude::wasm_bindgen;
 pub fn hydrate() {
     console_error_panic_hook::set_once();
     leptos::mount::hydrate_body(App);
